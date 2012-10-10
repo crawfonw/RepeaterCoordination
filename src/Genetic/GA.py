1f648e9225b1a3ebef5bcc2ae55207da7c19fbe1
Nick Crawford <crawfonw@rose-hulman.edu>
2012-10-10 16:39:11 -0400

    Working code

--- src/Genetic/GA.py
+++ src/Genetic/GA.py
@@ -1,6 +1,7 @@
+from copy import deepcopy
 from math import sqrt
 from operator import add
-import random
+from random import choice, randint, random
 
 def point_enclosed_by_circle(center, radius, p):
     return sqrt((center[0] - p[0])**2 + (center[1] - p[1])**2) <= radius
@@ -31,27 +32,36 @@ class Repeater():
     def __init__(self, xy, r): #xy is a 2-tuple
         self.loc = xy
         self.radius = r
+        
+    def __str__(self):
+        return 'Repeater at %s of r %s' % (self.loc, self.radius)
+        
+    __repr__ = __str__
 
 class GeneticAlgorithm():    
     def __init__(self, grid, pop_count):
         self.grid = grid
         self.population = self.generate_population(pop_count)
+        self.generation = 0
 
     def crossover(self, male, female, half):
-        return random.choice(male[:half] + female[half:], male[half:] + female[:half])
+        return choice([male[:half] + female[half:], male[half:] + female[:half]])
     
-    def individual(self):
+    def individual(self, repeater_radius = 5):
         repeaters = []
-        num_repeaters = random.randint(0, self.grid.size)
+        remaining_points = deepcopy(self.grid.points)
+        num_repeaters = randint(1, self.grid.size)
         for i in range(num_repeaters):
-            pass
+            point = choice(remaining_points)
+            remaining_points.remove(point)
+            repeaters.append(Repeater(point, repeater_radius))
+        return repeaters
             
     def generate_population(self, count):
         pop = []
         for i in range(count):
-            print '\nIndividual: %s' % (i + 1)
-            x = individual()
-            f = fitness(x)
+            x = self.individual()
+            f = self.fitness(x)
             pop.append((f, x))
         return pop
     
@@ -62,15 +72,21 @@ class GeneticAlgorithm():
             for point in self.grid.points:
                 if point_enclosed_by_circle(repeater.loc, repeater.radius, point):
                     points_covered += 1
-        
-        return min(points_covered, self.grid.size) / float(len(individual))
+        return 1.0 / (min(points_covered, self.grid.size) / float(len(individual)))
     
     def grade(self):
         summed = reduce(add, (x[0] for x in self.population))
         return summed / (len(self.population) * 1.0)
         
     def mutate(self, individual):
-        pass
+        remaining_points = deepcopy(self.grid.points)
+        for repeater in individual:
+            if repeater.loc in remaining_points:
+                remaining_points.remove(repeater.loc)
+        r = randint(1, len(individual)) - 1
+        point = choice(remaining_points)
+        individual[r].loc = point
+        return individual
     
     def evolve(self, retain=0.1, random_select=0.05, mutate=0.01):
         graded = [x for x in sorted(self.population)]
@@ -78,19 +94,19 @@ class GeneticAlgorithm():
         parents = graded[:retain_length]
         
         for individual in graded[retain_length:]:
-            if random_select > random.random():
+            if random_select > random():
                 parents.append(individual)
         
         for i in range(len(parents)):
-            if mutate > random.random():
+            if mutate > random():
                 mutated_individual = self.mutate(parents[i][1])
-                parents[i] = (fitness(mutated_individual), mutated_individual)
+                parents[i] = (self.fitness(mutated_individual), mutated_individual)
     
         desired_length = len(self.population) - len(parents)
         children = []
         while len(children) < desired_length:
-            male = random.choice(parents)[1]
-            female = random.choice(parents)[1]
+            male = choice(parents)[1]
+            female = choice(parents)[1]
             if male != female:
                 half = len(male) / 2
                 child = self.crossover(male, female, half)
@@ -98,8 +114,9 @@ class GeneticAlgorithm():
                 
         weighted_children = []
         for i, child in enumerate(children):
-            print '\nIndividual: %s' % (i + 1)
+            #print '\nIndividual: %s' % (i + 1)
             weighted_children.append((self.fitness(child), child))
         
         parents.extend(weighted_children)
         self.population = parents
+        self.generation += 1
--- src/Genetic/runGA.py
+++ src/Genetic/runGA.py
@@ -6,13 +6,19 @@ import time
 import os
 import zipfile
 
-POPULATION = 5
+POPULATION = 50
 GENERATIONS = 5
+
+for f in os.listdir(os.getcwd() + os.sep + 'results'):
+    if f.endswith('.txt'):
+        os.remove(os.getcwd() + os.sep + 'results' + os.sep + f)
 
 t1 = time.time()
 
-print 'Generation 0 - Population: %s\n' % POPULATION
-p = population(POPULATION)
+grid = Grid((10,10), 10)
+opt = GeneticAlgorithm(grid, POPULATION)
+
+print 'Generation %s - Population: %s\n' % (opt.generation, len(opt.population))
 
 t2 = time.time()
 
@@ -20,13 +26,18 @@ print 'Initial population calculation time: %0.3f min' % (float(t2 - t1) / 60.0)
 
 for i in range(1, GENERATIONS + 1):
     print '========================================'
-    print '\nOn generation %s - Population: %s\n' % (i, len(p))
+    print '\nOn generation %s - Population: %s\n' % (opt.generation, len(opt.population))
     
-    p = evolve(p, retain=0.1)
-    fitness_avg = grade(p)
-    print 'Score of this population: %s\n\n' % fitness_avg
+    opt.evolve()
+    fitness_avg = opt.grade()
+    avg_repeaters = (sum([len(x[1]) for x in opt.population]) / float(len(opt.population)))
+    print 'Score of this population: %s' % fitness_avg
+    print 'Avg. num repeaters: %s\n\n' % avg_repeaters
+    f = open(os.getcwd() + os.sep + 'results' + os.sep + 'generation%s_%s.txt' % (i, fitness_avg), 'w')
+    f.write('Score of this population: %s; Avg. Repeaters: %s\n\n' % (fitness_avg, avg_repeaters))
+    f.write('Population:\n\n')
     for i in p:
-        print 'Repeater at %s' % i
+        f.write('Score:%s\nRepeaters:%s\n\n' % (i[0], i[1]))
 
 t2 = time.time()
 #print 'Runtime: %0.3f sec' % float(t2 - t1)
